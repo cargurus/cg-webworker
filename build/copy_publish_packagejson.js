3780719acc78629cb15b943196f6a304671122ff
cg-amarginson <amarginson@cargurus.com>
2021-11-09 16:29:08 -0500

    Versioning (#10)
    * add versioning
    
    * copy npmrc as part of build
    
    * add action
    
    Co-authored-by: Alex Marg <6582665+gruckus-gruckus@users.noreply.github.com>

--- build/copy_publish_packagejson.js
+++ build/copy_publish_packagejson.js
@@ -1,21 +1,44 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.copyFile(
-    path.resolve(__dirname, './npm_dist_package.json'),
-    path.resolve(__dirname, '../dist/package.json'),
-    function (err) {
+function updatePublishVersion() {
+    fs.readFile(path.resolve(__dirname, '../dist/package.json'), 'utf8', (err, data) => {
         if (err) {
             // eslint-disable-next-line no-console
-            console.error('ERROR: ' + err);
+            console.error("ERROR: Couldn't read dist/package.json: " + err);
             process.exitCode = 1;
+        } else {
+            const packageJson = JSON.parse(data);
+            packageJson.version = process.env.PUBLISH_VERSION || process.env.npm_package_version;
+            fs.writeFile(path.resolve(__dirname, '../dist/package.json'), JSON.stringify(packageJson), (err) => {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.error("ERROR: Couldn't write updated version to dist/package.json: " + err);
+                    process.exitCode = 1;
+                }
+            });
         }
-    }
-);
+    });
+}
 
-// fs.rename('./npm_dist_package.json', '../dist/', function(err) {
-//     if (err) {
-//         console.error('ERROR: ' + err);
-//         process.exitCode = 1;
-//     }
-// });
+fs.copyFile(path.resolve(__dirname, '../.npmrc'), path.resolve(__dirname, '../dist/.npmrc'), function (err) {
+    if (err) {
+        // eslint-disable-next-line no-console
+        console.error("ERROR: couldn't copy npmrc " + err);
+        process.exitCode = 1;
+    } else {
+        fs.copyFile(
+            path.resolve(__dirname, './npm_dist_package.json'),
+            path.resolve(__dirname, '../dist/package.json'),
+            function (err) {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.error('ERROR: ' + err);
+                    process.exitCode = 1;
+                } else {
+                    updatePublishVersion();
+                }
+            }
+        );
+    }
+});
