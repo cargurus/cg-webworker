b243041648dc4f10dfbc0b7c7b715cf837cd0072
Alex Marg <6582665+gruckus-gruckus@users.noreply.github.com>
2021-11-09 15:10:02 -0500

    add versioning

--- build/copy_publish_packagejson.js
+++ build/copy_publish_packagejson.js
@@ -1,6 +1,26 @@
 const path = require('path');
 const fs = require('fs');
 
+function updatePublishVersion() {
+    fs.readFile(path.resolve(__dirname, '../dist/package.json'), 'utf8', (err, data) => {
+        if (err) {
+            // eslint-disable-next-line no-console
+            console.error("ERROR: Couldn't read dist/package.json: " + err);
+            process.exitCode = 1;
+        } else {
+            const packageJson = JSON.parse(data);
+            packageJson.version = process.env.PUBLISH_VERSION || process.env.npm_package_version;
+            fs.writeFile(path.resolve(__dirname, '../dist/package.json'), JSON.stringify(packageJson), (err) => {
+                if (err) {
+                    // eslint-disable-next-line no-console
+                    console.error("ERROR: Couldn't write updated version to dist/package.json: " + err);
+                    process.exitCode = 1;
+                }
+            });
+        }
+    });
+}
+
 fs.copyFile(
     path.resolve(__dirname, './npm_dist_package.json'),
     path.resolve(__dirname, '../dist/package.json'),
@@ -9,13 +29,8 @@ fs.copyFile(
             // eslint-disable-next-line no-console
             console.error('ERROR: ' + err);
             process.exitCode = 1;
+        } else {
+            updatePublishVersion();
         }
     }
 );
-
-// fs.rename('./npm_dist_package.json', '../dist/', function(err) {
-//     if (err) {
-//         console.error('ERROR: ' + err);
-//         process.exitCode = 1;
-//     }
-// });
